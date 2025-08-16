141166e6639fc068cd0a742d328f03f6d423f784
Sam <enthunt.sh@gmail.com>
2025-08-15 21:24:56 -0400

    added configs for babel, jest and react test lib

--- client/wolflite/src/assets/login/Main.jsx
+++ client/wolflite/src/assets/login/Main.jsx
@@ -1,41 +1,58 @@
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
 
 function Main() {
-
-  async function receieveData(e){
-    e.preventDefault()
+  async function receieveData(e) {
+    e.preventDefault();
     const form = e.target;
-    console.log(form)
-    const formData = new FormData(form)
+    const formData = new FormData(form);
 
     try {
-      const response = await fetch('/formSubmission', {
+      const request = await fetch("/formSubmission", {
         method: "POST",
         body: formData,
-      })
+      });
 
+      // parse the response as json
+      const response = await request.json();
+      console.log(response);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
 
-
   return (
     <>
-      <main id='loginContainer'>
-        <h1 className='loginTitle'>Wolf Lite</h1>
-        <form method="post" id='loginForm' onSubmit={receieveData}>
-          <input className="textBox" type="text" name='username' placeholder='Username' required/>
+      <main id="loginContainer">
+        <h1 className="loginTitle">Wolf Lite</h1>
+        <form method="post" id="loginForm" onSubmit={receieveData}>
+          <input
+            className="textBox"
+            type="text"
+            name="username"
+            placeholder="Username"
+            required
+          />
           <br />
-          <input className="textBox" type="password" name='password' placeholder='Password' required/>
-          <div id='submitBtnGroup'>
-            <button type='submit' className='submitBtn' >Create User</button>
-            <button type='submit' className='submitBtn' >Login</button>
+          <input
+            className="textBox"
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+          />
+          <div id="submitBtnGroup">
+            <button type="submit" className="submitBtn">
+              Create User
+            </button>
+            <button type="submit" className="submitBtn">
+              Login
+            </button>
           </div>
         </form>
       </main>
     </>
-  )
+  );
 }
 
-export default Main
+export default Main;
---
+++ client/wolflite/tests/Main.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render, fireEvent, screen} from '@testing-library/react'
+import Main from '../src/assets/login/Main';
+import '@testing-library/jest-dom';
+
+test('renders login form', ()=> {
+   render(<Main/>);
+   expect(screen.getByPlaceholderText(/Username/i)).toBeInTheDocument()
+   expect(screen.getByPlaceholderText(/Password/i)).toBeInTheDocument()
+})
