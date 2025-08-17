8bd1c30e83fdbb3151e9f9d84498258c819df358
Sam <enthunt.sh@gmail.com>
2025-08-17 02:58:36 -0400

    added a new expectation for the login form test

--- client/wolflite/tests/Main.test.jsx
+++ client/wolflite/tests/Main.test.jsx
@@ -3,8 +3,11 @@ import {render, fireEvent, screen} from '@testing-library/react'
 import Main from '../src/assets/login/Main';
 import '@testing-library/jest-dom';
 
-test('renders login form', ()=> {
+test('renders login form and hyperlinks inside', ()=> {
    render(<Main/>);
    expect(screen.getByPlaceholderText(/Username/i)).toBeInTheDocument()
    expect(screen.getByPlaceholderText(/Password/i)).toBeInTheDocument()
+
+   // Hyperlinks
+   expect(screen.getByRole('link')).toHaveAttribute('href', '/login')
 })
