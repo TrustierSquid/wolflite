741a2da65aca6604ca8523f8cf44cd944b41ce7e
Sam <enthunt.sh@gmail.com>
2025-08-19 05:35:21 -0400

    added another test for login form

--- server/wolfr/tests/test_loginForm.py
+++ server/wolfr/tests/test_loginForm.py
@@ -6,7 +6,12 @@ def client():
    app = create_app({'TESTING': True})
    return app.test_client()
 
-def test_form_submission(client):
+# FORMS
+
+def test_form_create_user(client):
    response = client.post('/formSubmission', data={'username': 'test', 'password': 'pass'})
    assert response.status_code == 200
-   assert response.get_json()['message'] == 'Received'
+
+def test_form_login(client):
+   response = client.post('/loginUser', data={'username': 'test', 'password': 'pass'})
+   assert response.status_code == 200
