fe74e81963a74cffacb9c495973c377db68e79d0
Sam <enthunt.sh@gmail.com>
2025-08-26 01:36:30 -0400

    restyled post author text and added timestamps for each post

--- client/wolflite/src/assets/blogComponents/BlogFeed.jsx
+++ client/wolflite/src/assets/blogComponents/BlogFeed.jsx
@@ -31,10 +31,21 @@ export default function BlogFeed() {
     <main id="blogFeedContainer">
       {
         allPosts?.posts.map((post) => {
+          const date = new Date(post.created)
+          const formattedDate = date.toLocaleString("en-US", {
+            // weekday: "long",
+            month: "short",
+            day: "numeric",
+            hour: "numeric",
+            minute: "numeric",
+            hour12: true,
+            year: "numeric"
+          })
+
           return (
             <>
               <section className="postContainer">
-                <span className="postAuthor">{post.username}</span>
+                <h5 className="postAuthor">{post.username} <span>{formattedDate}</span></h5>
                 <h2>{post.title}</h2>
                 <p>{post.body}</p>
               </section>
@@ -42,6 +53,12 @@ export default function BlogFeed() {
           )
         })
       }
+
+      <section className="postContainer">
+        <span className="postAuthor">person</span>
+        <h2>Lorem ipsum dolor sit amet.</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque iste blanditiis nisi provident officiis recusandae magnam ipsum odit quos omnis laboriosam natus nam aperiam, dignissimos nobis harum quidem dolore distinctio minus soluta, eligendi ut commodi architecto accusantium? Optio, possimus esse cumque, cum blanditiis earum accusantium tempora illum beatae quam hic.</p>
+      </section>
 
     </main>
   );
