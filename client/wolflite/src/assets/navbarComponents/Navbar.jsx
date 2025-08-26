9ab9619225c39d3034f022f05ae1097171420e52
Sam <enthunt.sh@gmail.com>
2025-08-26 03:16:37 -0400

    added a click event to the navbar for easy access back to the login page

--- client/wolflite/src/assets/navbarComponents/Navbar.jsx
+++ client/wolflite/src/assets/navbarComponents/Navbar.jsx
@@ -3,14 +3,14 @@ import React from "react";
 function Navbar() {
   return (
     <nav id="navContainer">
-      <section className="navContainerSection">
+      <section className="navContainerSection" onClick={()=> window.location.href = '/'}>
         <img id="navImg" src="./src/assets/imgs/wolfLogo.png" alt="" />
         <h2>WOLFLITE</h2>
       </section>
 
       <ul className="navContainerSectionLinks">
         <li>
-          <a href="#">Post</a>
+          <a href="#" onClick={()=> {window.location.href = '/create'; console.log("hello")}}>Post</a>
         </li>
         <li>
           <a href="#">Changelog</a>
