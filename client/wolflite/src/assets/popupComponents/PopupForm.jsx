6d1bc4d7c205e98e3ee09ca66be9d5faff70eca5
Sam <enthunt.sh@gmail.com>
2025-08-28 00:35:45 -0400

    Styled the 'create post' text moodle option

--- client/wolflite/src/assets/popupComponents/PopupForm.jsx
+++ client/wolflite/src/assets/popupComponents/PopupForm.jsx
@@ -15,7 +15,7 @@ export default function PopupForm(){
 
 
          <main id="elementContainer">
-            <h2 className="postTitle">Create Post</h2>
+            <h2 className="moodleTitle">Create Post</h2>
 
             <div id="createMenuContainer">
                <ul id="createMenuOptions">
@@ -23,21 +23,43 @@ export default function PopupForm(){
                   <li onClick={()=> setMenuSelector('Image')}><h3>Image</h3></li>
                   <li onClick={()=> setMenuSelector('Poll')}><h3>Poll</h3></li>
                </ul>
-               <section id="createMenuContent">
-                  <h2>Text</h2><br />
-                  <form action="/post/create" method="POST" id="createPostForm">
-                     <div className="formPostTitleSection">
-                        <label className="createPostFormLabel" for="postTitle">Post Title:</label><br />
-                        <input className="createPostFormInputTitle" name="postTitle" type="text" placeholder="Enter a post title" />
-                     </div>
 
-                     <div className="formPostBodySection">
-                        <label className="createPostFormLabel" for="postContent">Post Body:</label><br />
-                        <textarea className="createPostFormInput" type="text" name="postContent" placeholder="Enter a post Body"/>
-                     </div>
-                     <button type="submit">Post</button>
-                  </form>
-               </section>
+               {
+                  menuSelector === 'Image' ? (
+                     <section id="createMenuContent">
+                        <h2>Image</h2><br />
+                        <form action="/post/create" method="POST" id="createPostForm">
+                           <div className="formPostTitleSection">
+                              <label className="createPostFormLabel" for="postTitle">Post Title:</label><br />
+                              <input className="createPostFormInputTitle" name="postTitle" type="text" placeholder="Enter a post title" />
+                           </div>
+
+                           <div className="formPostBodySection">
+                              <label className="createPostFormLabel" for="postContent">Post Body:</label><br />
+                              <textarea className="createPostFormInput" type="text" name="postContent" placeholder="Enter a post Body"/>
+                           </div>
+                           <button type="submit">Post</button>
+                        </form>
+                     </section>
+                  ) : (
+                     <section id="createMenuContent">
+                        <h2>Text</h2><br />
+                        <form action="/post/create" method="POST" id="createPostForm">
+                           <div className="formPostTitleSection">
+                              <label className="createPostFormLabel" for="postTitle">Post Title:</label><br />
+                              <input className="createPostFormInputTitle" name="postTitle" type="text" placeholder="Enter a post title" />
+                           </div>
+
+                           <div className="formPostBodySection">
+                              <label className="createPostFormLabel" for="postContent">Post Body:</label><br />
+                              <textarea className="createPostFormInput" type="text" name="postContent" placeholder="Enter a post Body"/>
+                           </div>
+                           <button type="submit" id="submitTextBtn">Post</button>
+                        </form>
+                     </section>
+                  )
+               }
+
             </div>
          </main>
 
