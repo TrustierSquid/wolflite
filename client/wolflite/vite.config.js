c1cd69576117ea495cb5db6deb641366d5cc2fce
Sam <enthunt.sh@gmail.com>
2025-09-20 22:21:31 -0400

    Added profile in the input object for when the developer runs 'npm run build'.  This bundles the profile.html in with the static files that get bundled.

--- client/wolflite/vite.config.js
+++ client/wolflite/vite.config.js
@@ -23,7 +23,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         blog: 'blog.html',
-        create: 'create.html'
+        create: 'create.html',
+        profile: 'profile.html',
       }
     }
   }
