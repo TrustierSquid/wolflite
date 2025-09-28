3261f27b6da82817b65a50d464a6f2dc2717a56c
Sam <enthunt.sh@gmail.com>
2025-09-28 15:20:37 -0400

    Uncommented the 'base' key for production
    Devs can comment this out if they are working on the vite dev server

--- client/wolflite/vite.config.js
+++ client/wolflite/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/static/dist/', /* Comment out if you are working on vite dev server */
+  base: '/static/dist/', /* Comment out if you are working on vite dev server */
   server: {
     proxy: {
       '/formSubmission': 'http://localhost:5000',
